959cf573dac36663606608ef1d7c4d9bd53e3700
JoseMFT <jomaft03@gmail.com>
2022-11-10 08:52:57 +0000

    10/11/2022 - 8:52
    Subida archivos sin modificar, desde el repositorio de Eliseo

---
+++ TiroAlCilindro-main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Material hitMaterial;
+    public AudioClip shotSound;
+    private AudioSource gunAudioSource;
+
+    void Awake()
+    {
+        gunAudioSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if((Input.touchCount >= 1 && Input.GetTouch(0).phase == TouchPhase.Ended) || (Input.GetMouseButtonUp(0)))
+        {
+            gunAudioSource.PlayOneShot(shotSound);
+            Vector3 pos = Input.mousePosition;
+            if (Application.platform == RuntimePlatform.Android)
+            { 
+                pos = Input.GetTouch(0).position;
+            }
+
+            Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayo, out hitInfo))
+            {
+                if (hitInfo.collider.tag.Equals("Lata"))
+                {
+                    Rigidbody rigidbodyLata = hitInfo.collider.GetComponent<Rigidbody>();
+                    rigidbodyLata.AddForce(rayo.direction * 50f, ForceMode.VelocityChange);
+                    hitInfo.collider.GetComponent<MeshRenderer>().material = hitMaterial;
+                }
+                
+            
+            }
+        
+        }
+    }
+}
