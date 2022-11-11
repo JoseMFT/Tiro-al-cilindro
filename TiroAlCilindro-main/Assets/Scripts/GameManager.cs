0d4fc6ee75f62afb8e794b31bad95c49b47fd6c7
JoseMFT <jomaft03@gmail.com>
2022-11-11 00:33:05 +0000

    11/11/2022 - 00:31
    Se bugeó el generador de latas.
    Las texturas no cambian al clicar en la lata, tampoco estas se destruyen

--- TiroAlCilindro-main/Assets/Scripts/GameManager.cs
+++ TiroAlCilindro-main/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using TMPro;
 
 public class GameManager: MonoBehaviour {
-    public Material hitMaterial;
+    //public Material hitMaterial;
     public AudioClip shotCan, shotAir, shotSound, reloadGun, gunCocking;
     public Canvas canvasUI;
     private AudioSource gunAudioSource;
@@ -19,6 +19,7 @@ public class GameManager: MonoBehaviour {
 
     void Awake () {
         gunAudioSource = GetComponent<AudioSource> ();
+
     }
     // Update is called once per frame
     void Update () {
@@ -37,8 +38,8 @@ public class GameManager: MonoBehaviour {
         // Si el número de veces clicado en pantalla es mayor o igual que uno Y se ha dejado de pulsar el clic izquierdo, o si se ha dejado de pulsar el botón:
         if (((Input.touchCount >= 1 && Input.GetTouch (0).phase == TouchPhase.Ended) || (Input.GetMouseButtonUp (0)) && ammo >= 1)) // (0 = botón izq., 1 = botón cent., 2= botón der.)
         {
-            ammo--;
             gunAudioSource.PlayOneShot (shotSound);
+            ammo--;
             Vector3 pos = Input.mousePosition;
             // Se ejecuta si el juego está corriendo en Android
             if (Application.platform == RuntimePlatform.Android) {
@@ -49,32 +50,26 @@ public class GameManager: MonoBehaviour {
             RaycastHit hitInfo;
             if (Physics.Raycast (rayo, out hitInfo)) {
                 if (hitInfo.collider.tag.Equals ("Lata")) {
-
                     gunAudioSource.PlayOneShot (shotCan);
                     score += 10;
                     Rigidbody rigidbodyLata = hitInfo.collider.GetComponent<Rigidbody> ();
                     rigidbodyLata.AddForce (rayo.direction * shotStrength, ForceMode.VelocityChange);
                     //hitInfo.collider.GetComponent<MeshRenderer> ().material = hitMaterial;
+
                 } else if (hitInfo.collider.tag.Equals ("Vacio")) {
                     gunAudioSource.PlayOneShot (shotAir);
                     score -= 5;
                 }
             }
-        } else if ((ammo <= 0) && reloadTime == 2.5f) {
-            reloading ();
-        }
-    }
-
-    void reloading () {
-        gunAudioSource.PlayOneShot (reloadGun);
-        while (reloadTime >= 0.0f) {
+        } else if ((ammo <= 0) && reloadTime >= 0.0f) {
             reloadTime -= Time.deltaTime;
-        }
-        if (reloadTime <= 0.0f) {
-            ammo = 6;
-            gunAudioSource.PlayOneShot (gunCocking);
+            
+        } else if ((ammo <= 0) && reloadTime <= 0.0f) {
             reloadTime = 2.5f;
-
+            gunAudioSource.PlayOneShot (gunCocking);
+            ammo = 6;
+        } else if ((ammo <= 0) && reloadTime >= 2.5f) {
+            gunAudioSource.PlayOneShot (reloadGun);
         }
     }
 }
